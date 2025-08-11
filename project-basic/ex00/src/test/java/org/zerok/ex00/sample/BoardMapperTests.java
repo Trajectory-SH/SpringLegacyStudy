18513879df2293bff5c9a4f4915a69a4bdb687be
Trajectory-SH <trajectory0611@gmail.com>
2025-08-11 13:41:41 +0900

    🚧 List BoardVO

---
+++ project-basic/ex00/src/test/java/org/zerok/ex00/sample/BoardMapperTests.java
@@ -0,0 +1,31 @@
+package org.zerok.ex00.sample;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.zerock.ex00.mappers.BoardMapper;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j2
+public class BoardMapperTests {
+    @Autowired(required = false)
+    BoardMapper boardMapper;
+
+    @Test
+    public void test1() {
+        log.info(boardMapper);
+    }
+
+    @Test
+    public void testList() {
+
+        boardMapper.getList().forEach(boardVO -> log.info(boardVO));
+    }
+
+
+
+}
